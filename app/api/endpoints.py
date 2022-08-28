014e2bb468115e7f7137e98ff46836731e9652c3
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2022-08-28 07:11:48 +0530

    Clean up code

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -28,27 +28,3 @@ class ActiveUser(Resource):
             "display_name": user.get_name(),
             "email": user.email
         }
-
-
-
-
-# class GetAllAnnouncements(Resource):
-#     def get(self):
-#         """get 
-        
-#         Get all the announcements, in a JSON form. Then in static site,
-#         we will add a parser to render like in achievements.
-#         """
-#         # return jsonify.
-#         return_obj = []
-#         for ann in Announcement.get_all():
-#             return_obj.append({
-#                 # "author_username" : ann.author.username,
-#                 "title" : ann.title,
-#                 "body" : ann.body,
-#                 "timestamp" : ann.timestamp.strftime("%Y-%m-%d %H:%M:%S")
-#             })
-        
-#         return return_obj
-
-# app_api.add_resource(GetAllAnnouncements, "/announcements")
